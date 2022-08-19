118302e3bd1e1c8c477f30a9f54d40607539a620
guillobr <guillermobr88@gmail.com>
2022-08-19 11:28:41 -0300

    A

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,10 @@ function App() {
       <header className="App-header">
         <img src='https://thumbs.dreamstime.com/b/jaguar-onca-del-panthera-aislado-43168559.jpg' className="App-logo" alt="logo" />
         <p>
-          ALIANZA BINACIONAL
-        
-        
-          JAGUARES
+          ALIANZA BINACIONAL  JAGUARES
        </p>
+       <p>AVISTAJE</p>
+       <p>TRAFICO</p>
       </header>
     </div>
   );
